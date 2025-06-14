655360a6d79f7bff461ba24eb1f1da0c45ce625c
SantiG95 <santiguerra86@gmail.com>
2025-06-14 19:42:17 -0300

    Creada la pagina de recomendaciones

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { UploadPhoto } from "./pages/UploadPhoto";
 import { VirtualTryOn } from "./pages/VirtualTryOn";
 import { Profile } from "./pages/Profile";
 import { Catalog } from "./pages/Catalog";
+import { Recommendations } from "./pages/Recommendations";
 import { Routes, Route } from "react-router-dom";
 
 export const App = () => {
@@ -25,6 +26,7 @@ export const App = () => {
         <Route path="/virtual-try-on" element={<VirtualTryOn />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/catalog" element={<Catalog />} />
+        <Route path="/recommendations" element={<Recommendations/>} />
       </Routes>
     </div>
   );
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,47 +5,38 @@ const imagenes = import.meta.glob('../img/*.{png,jpg,jpeg}', { eager: true, impo
 
 
 interface CartaProps {
-    id: number;
     nombre: string;
     fecha: string;
     imagen: string;
 }
 
-const Card: React.FC<CartaProps> = ({id, imagen, nombre, fecha}) => {
-    return (
-        <div className="bg-slate-800/50 rounded-lg overflow-hidden group transition-transform duration-300 hover:-translate-y-1 cursor-pointer">
-            <a href={"catalogue/"+id}>
-                {/* Recuadro para la imagen */}
-                <div className="aspect-[3/4] w-full bg-slate-700 flex items-center justify-center">
-                    {imagen === "" ? (
-                        <ImageIcon className="h-12 w-12 text-slate-500 group-hover:text-purple-400 transition-colors select-none" />
-                    ) : (
-                        <img src={decidirDireccionImagen(imagen)} alt="" className="h-full w-full object-cover select-none"/>
-                    )}
-                </div>
-            
-                {/* Contenido de texto debajo de la imagen */}
-                <div className="p-4">
-                    <p className="font-semibold text-white truncate select-none" title={nombre}>
-                        {nombre}
-                    </p>
-                    <p className="text-sm text-gray-400 mt-1 select-none">
-                        {fecha}
-                    </p>
-                </div>
-            </a>
-        </div>
-    )
-}
-
-function decidirDireccionImagen(imagen:string){
-    //Si la imagen esta en internet
-    if(imagen.startsWith("https")) return imagen;
-
-    //Si la imagen esta en local
-    return Object.entries(imagenes).find(([path]) =>
+const Card: React.FC<CartaProps> = ({imagen, nombre, fecha}) => {
+    const ruta = Object.entries(imagenes).find(([path]) =>
     path.includes(`/img/${imagen}.`)
     )?.[1];
+
+    return (
+        <div>
+            {/* Recuadro para la imagen */}
+            <div className="aspect-[3/4] w-full bg-slate-700 flex items-center justify-center">
+                {imagen === "" ? (
+                    <ImageIcon className="h-12 w-12 text-slate-500 group-hover:text-purple-400 transition-colors" />
+                ) : (
+                    <img src={ruta} alt="" className="h-full w-full object-cover"/>
+                )}
+            </div>
+        
+            {/* Contenido de texto debajo de la imagen */}
+            <div className="p-4">
+                <p className="font-semibold text-white truncate" title={nombre}>
+                    {nombre}
+                </p>
+                <p className="text-sm text-gray-400 mt-1">
+                    {fecha}
+                </p>
+            </div>
+        </div>
+    )
 }
 
 export default Card;
